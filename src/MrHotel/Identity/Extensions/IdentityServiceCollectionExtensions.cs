2f137a3ef23f4d31ea1da3974c9b33b59ff3fd52
RaptorRush <raptorrush135@hotmail.com>
2024-10-02 01:57:31 -0600

    Remove password requirements. Set min length to 15

--- src/MrHotel/Identity/Extensions/IdentityServiceCollectionExtensions.cs
+++ src/MrHotel/Identity/Extensions/IdentityServiceCollectionExtensions.cs
@@ -11,10 +11,17 @@ public static class IdentityServiceCollectionExtensions
     public static void AddAppIdentity<TDbContext>(this IServiceCollection services)
         where TDbContext : IdentityDbContext<AppUser>
     {
-        services.AddIdentityCore<AppUser>(options => options.Password.RequiredLength = 10)
-            .AddEntityFrameworkStores<TDbContext>()
-            .AddUserManager<UserManager<AppUser>>()
-            .AddSignInManager<SignInManager<AppUser>>()
-            .AddMrHotelApiEndpoints();
+        services.AddIdentityCore<AppUser>(options =>
+        {
+            options.Password.RequiredLength = 15;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireDigit = false;
+        })
+        .AddEntityFrameworkStores<TDbContext>()
+        .AddUserManager<UserManager<AppUser>>()
+        .AddSignInManager<SignInManager<AppUser>>()
+        .AddMrHotelApiEndpoints();
     }
 }
