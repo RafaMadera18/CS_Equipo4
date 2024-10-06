0279b4645def470f631df39819d87a5d8904499c
RafaMadera18 <rafa_madlop@hotmail.com>
2024-10-06 11:44:17 -0600

    Add Base Style App

--- src/MrHotel/WebApp/src/app/pages/menu/menu.page.ts
+++ src/MrHotel/WebApp/src/app/pages/menu/menu.page.ts
@@ -21,7 +21,7 @@ import {
 import { TabButtonComponent } from "@components/tab-button";
 
 import { addIcons } from "ionicons";
-import { reorderThreeOutline, bedOutline, calendarOutline } from "ionicons/icons";
+import { reorderThreeOutline, bedOutline, calendarOutline, arrowBackOutline} from "ionicons/icons";
 
 @Component({
   selector: "app-menu",
@@ -50,7 +50,7 @@ export class MenuPage {
   protected readonly tabs = viewChild.required(IonTabs);
 
   constructor() {
-    addIcons({ reorderThreeOutline, bedOutline, calendarOutline });
+    addIcons({ reorderThreeOutline, bedOutline, calendarOutline, arrowBackOutline });
 
     effect(() => {
       this.sidebar().nativeElement.classList.toggle(
