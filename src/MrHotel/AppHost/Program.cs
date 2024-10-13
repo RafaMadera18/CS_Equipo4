8b78183935e0f6e97157be883f02d4ad25993ee0
RaptorRush <raptorrush135@hotmail.com>
2024-10-12 19:21:37 -0600

    Update EFMigrationService

--- src/MrHotel/AppHost/Program.cs
+++ src/MrHotel/AppHost/Program.cs
@@ -1,8 +1,12 @@
 using EFMigrationService.Integration;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 using MrHotel.AppHost;
 
 using RaptorUtils.Aspire.Hosting.NodeJs;
+using RaptorUtils.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -21,13 +25,23 @@ if (!builder.Configuration.IsDatabaseMigrationMode())
 }
 else
 {
+    using var serviceProvider = builder.Services.BuildServiceProvider();
+
+    var logger = serviceProvider.GetRequiredService<ILoggerFactory>().CreateLogger<MigrationService>();
+
+    string efToolVersion = builder.Configuration.GetRequired("EFToolVersion");
+
+    var migrationService = new MigrationService(logger, "../../EFMigrationService", efToolVersion);
+
+    await migrationService.Initialize();
+
     string migrationTargetProject = Path.GetFullPath("../Database");
 
     var migrationServer = builder.AddProject<Projects.EFMigrationService_Server>("MigrationServer")
         .WithReference(mrHotelDb)
         .WithSingleMigrationProject(migrationTargetProject);
 
-    builder.AddMigrationClient(migrationServer, "../../EFMigrationService/Client");
+    builder.AddMigrationClient(migrationServer.GetEndpoint("http"), migrationService.ClientPath);
 }
 
 await builder.Build().RunAsync();
