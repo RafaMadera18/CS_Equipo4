c92f83c61e6bd5bf8ab3eb1aab73dd28b8ea555f
RafaMadera18 <rafa_madlop@hotmail.com>
2024-10-27 18:45:37 -0600

    Format Guests Page

--- src/MrHotel/WebApp/src/app/pages/guests/guests.page.ts
+++ src/MrHotel/WebApp/src/app/pages/guests/guests.page.ts
@@ -1,55 +1,48 @@
-import { Component} from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Guest } from '@services/guest-manager/data';
-import { Guid } from '@customTypes/guid';
-import { CommonModule } from '@angular/common';
-import { addIcons } from 'ionicons';
-import { pencilOutline, trashOutline, personAddOutline } from 'ionicons/icons';
-import { IonicModule } from '@ionic/angular';
+import { Component } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Guest } from "@services/guest-manager/data";
+import { Guid } from "@customTypes/guid";
+import { CommonModule } from "@angular/common";
+import { addIcons } from "ionicons";
+import { pencilOutline, trashOutline, personAddOutline } from "ionicons/icons";
+import { IonicModule } from "@ionic/angular";
 
 @Component({
-  selector: 'app-guests',
-  templateUrl: './guests.page.html',
-  styleUrls: ['./guests.page.scss'],
+  selector: "app-guests",
+  templateUrl: "./guests.page.html",
+  styleUrls: ["./guests.page.scss"],
   standalone: true,
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule],
 })
 export class GuestsPage {
-
   protected readonly guests: Observable<Guest[]>;
 
   constructor() {
-    addIcons({pencilOutline, trashOutline, personAddOutline});
+    addIcons({ pencilOutline, trashOutline, personAddOutline });
     // TODO: Change for Guest Service
     this.guests = of([
       {
         id: "1" as Guid,
-        name: 'John Doe',
-        phoneNumber: '123-456-7890',
-        dateOfBirth: new Date(1990, 1, 15)
+        name: "John Doe",
+        phoneNumber: "123-456-7890",
+        dateOfBirth: new Date(1990, 1, 15),
       },
       {
         id: "2" as Guid,
-        name: 'Jane Smith',
-        phoneNumber: '234-567-8901',
-        dateOfBirth: new Date(1985, 5, 24)
+        name: "Jane Smith",
+        phoneNumber: "234-567-8901",
+        dateOfBirth: new Date(1985, 5, 24),
       },
       {
         id: "3" as Guid,
-        name: 'Alice Johnson',
-        phoneNumber: '345-678-9012',
-        dateOfBirth: new Date(2000, 10, 2)
-      }
+        name: "Alice Johnson",
+        phoneNumber: "345-678-9012",
+        dateOfBirth: new Date(2000, 10, 2),
+      },
     ]);
   }
 
-  public async addGuest(): Promise<void> {
+  public async addGuest(): Promise<void> {}
 
-  }
-
-  public async deleteGuest(guest: Guest): Promise<void> {
-
-  }
-
-
+  public async deleteGuest(guest: Guest): Promise<void> {}
 }
