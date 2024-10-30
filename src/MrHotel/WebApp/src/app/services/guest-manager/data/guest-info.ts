ee5b6a3f7ce0dc3a39c83ee71a4867053eca243e
Monse1011 <monseanayaalva@gmail.com>
2024-10-30 10:22:57 -0600

    From public to private

--- src/MrHotel/WebApp/src/app/services/guest-manager/data/guest-info.ts
+++ src/MrHotel/WebApp/src/app/services/guest-manager/data/guest-info.ts
@@ -1,12 +1,47 @@
 import { Guid } from "@customTypes/guid";
 
 export class GuestInfo {
+
+  private readonly _id: Guid;
+  private _fullName: string;
+  private _phoneNumber: string;
+  private _dateOfBirth: Date;
+
   public constructor(
-    public readonly id: Guid,
-    public fullName: string,
-    public phoneNumber: string,
-    public dateOfBirth: Date,
-  ) {}
+    id: Guid,
+    fullName: string,
+    phoneNumber: string,
+    dateOfBirth: Date,
+  ) {
+    this._id = id;
+    this._fullName = fullName;
+    this._phoneNumber = phoneNumber;
+    this._dateOfBirth = dateOfBirth;
+  }
+
+  public get id(): Guid {
+    return this._id;
+  }
+
+  public get fullName(): string {
+    return this._fullName;
+  }
+
+  public set fullName(value: string) {
+    this._fullName = value;
+  }
+
+  public get phoneNumber(): string {
+    return this._phoneNumber;
+  }
+
+  public set phoneNumber(value: string) {
+    this._phoneNumber = value;
+  }
+
+  public get dateOfBirth(): Date {
+    return this._dateOfBirth;
+  }
 
   public static createFromDto(dto: GuestInfoDto): GuestInfo {
     return new GuestInfo(
@@ -21,3 +56,4 @@ export class GuestInfo {
 export type GuestInfoDto = Omit<GuestInfo, "dateOfBirth"> & {
   dateOfBirth: string;
 };
+
