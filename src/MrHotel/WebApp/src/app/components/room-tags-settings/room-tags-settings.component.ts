91e4e90e4d4129452b86a176f1dd4b73c6e4f345
RafaMadera18 <rafa_madlop@hotmail.com>
2024-11-14 20:10:40 -0600

    Add Room Tags Settings Advance 1

--- src/MrHotel/WebApp/src/app/components/room-tags-settings/room-tags-settings.component.ts
+++ src/MrHotel/WebApp/src/app/components/room-tags-settings/room-tags-settings.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
+import { IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from "ionicons";
+import { addOutline } from "ionicons/icons";
 
 @Component({
   selector: "app-room-tags-settings",
   templateUrl: "./room-tags-settings.component.html",
   styleUrls: ["./room-tags-settings.component.scss"],
   standalone: true,
+  imports: [IonIcon],
 })
 export class RoomTagsSettingsComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ addOutline });
+  }
 }
