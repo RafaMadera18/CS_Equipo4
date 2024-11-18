5ba170e108c9e294b274b125a9e4ddccf9b9d360
RafaMadera18 <rafa_madlop@hotmail.com>
2024-11-17 18:41:38 -0600

    Add Input RoomPropertyGroup

--- src/MrHotel/WebApp/src/app/components/room-property-group/room-property-group.component.ts
+++ src/MrHotel/WebApp/src/app/components/room-property-group/room-property-group.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input } from "@angular/core";
+import { RoomPropertyGroup } from "@services/room-property-group-manager/data";
 
 @Component({
-  selector: 'app-room-property-group',
-  templateUrl: './room-property-group.component.html',
-  styleUrls: ['./room-property-group.component.scss'],
+  selector: "app-room-property-group",
+  templateUrl: "./room-property-group.component.html",
+  styleUrls: ["./room-property-group.component.scss"],
 })
-export class RoomPropertyGroupComponent  implements OnInit {
+export class RoomPropertyGroupComponent {
+  _roomProperty = input<RoomPropertyGroup>();
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor() {}
 }
