0323d5938408d8f2820f9ee2a4fdbdbe3a7438cc
RaptorRush <raptorrush135@hotmail.com>
2024-11-19 21:45:26 -0600

    RoomEndpoints: Add Properties to PUT and GET

--- src/MrHotel/ApiService/Rooms/Data/RoomUpdateData.cs
+++ src/MrHotel/ApiService/Rooms/Data/RoomUpdateData.cs
@@ -2,10 +2,30 @@
 
 using MrHotel.Database.Entities.Rooms;
 
-public record RoomUpdateData(string Name)
+using RaptorUtils.Collections.Extensions;
+
+public record RoomUpdateData(
+    string Name,
+    IReadOnlyCollection<Guid> PropertiesIds)
 {
     public void Update(RoomInfo room)
     {
         room.Name = this.Name;
+
+        this.UpdateProperties(room.Properties);
+    }
+
+    private void UpdateProperties(ICollection<RoomProperty> properties)
+    {
+        properties.Clear();
+
+        IEnumerable<RoomProperty> newProperties = this.GetNewProperties();
+        properties.AddRange(newProperties);
+    }
+
+    private IEnumerable<RoomProperty> GetNewProperties()
+    {
+        return this.PropertiesIds
+            .Select(id => new RoomProperty() { Id = id });
     }
 }
