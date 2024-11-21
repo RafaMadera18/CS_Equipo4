01aaaa222ff1a0c82846f3bd230192afe270928d
RafaMadera18 <rafa_madlop@hotmail.com>
2024-11-21 13:52:00 -0600

    Ups

--- src/MrHotel/WebApp/src/app/services/inventory-manager/data/product-stock-creation-data.ts
+++ src/MrHotel/WebApp/src/app/services/inventory-manager/data/product-stock-creation-data.ts
@@ -6,8 +6,9 @@ import { Stringify } from "@customTypes/stringify";
 export class ProductStockCreationData {
   public constructor(
     private readonly _productName: string,
-    private readonly _stockQuantity: number,
     private readonly _idealQuantity: number,
+    private readonly _stockQuantity: number,
+
   ) {}
 
   get productName(): string {
