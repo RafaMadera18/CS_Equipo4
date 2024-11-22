aa3d4e27c736bf4bf77fc2c2f3c7ff61c5257e78
RaptorRush <raptorrush135@hotmail.com>
2024-11-21 23:39:30 -0600

    Fix room update

---
+++ src/MrHotel/ApiService/Rooms/Data/RoomUpdate.cs
@@ -0,0 +1,18 @@
+﻿namespace MrHotel.ApiService.Rooms.Data;
+
+using MrHotel.Database.Entities.Rooms;
+
+using RaptorUtils.Collections.Extensions;
+
+public record RoomUpdate(
+    string Name,
+    IReadOnlyCollection<RoomProperty> Properties)
+{
+    public void ApplyUpdate(RoomInfo room)
+    {
+        room.Name = this.Name;
+
+        room.Properties.Clear();
+        room.Properties.AddRange(this.Properties);
+    }
+}
