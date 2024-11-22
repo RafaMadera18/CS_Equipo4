a0d24e193a5b25de7cf84e6b787463b43dd86ed0
RaptorRush <raptorrush135@hotmail.com>
2024-11-22 00:34:36 -0600

    Database: JsonIgnore foreign keys

--- src/MrHotel/Database/Entities/Reports/StockAdjustment.EF.cs
+++ src/MrHotel/Database/Entities/Reports/StockAdjustment.EF.cs
@@ -1,6 +1,9 @@
 namespace MrHotel.Database.Entities.Reports;
 
+using System.Text.Json.Serialization;
+
 public partial class StockAdjustment
 {
+    [JsonIgnore]
     public Guid ProductId { get; init; }
 }
--- src/MrHotel/Database/Entities/Reservations/ReservationInfo.EF.cs
+++ src/MrHotel/Database/Entities/Reservations/ReservationInfo.EF.cs
@@ -1,8 +1,12 @@
 ﻿namespace MrHotel.Database.Entities.Reservations;
 
+using System.Text.Json.Serialization;
+
 public partial class ReservationInfo
 {
+    [JsonIgnore]
     public Guid GuestId { get; set; }
 
+    [JsonIgnore]
     public Guid RoomId { get; set; }
 }
